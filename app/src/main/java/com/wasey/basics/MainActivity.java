abb79cb0c08a4111ca4e018263131e0ac88f9afd
Abdul Wasey Butt <abutt07@L-689.i2c.com>
2020-10-14 15:25:43 +0500

    custom toast added

--- app/src/main/java/com/wasey/basics/MainActivity.java
+++ app/src/main/java/com/wasey/basics/MainActivity.java
@@ -3,6 +3,12 @@ package com.wasey.basics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        makeCustomToastAppear();
+    }
+
+    private void makeCustomToastAppear(){
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView textView = (TextView) layout.findViewById(R.id.text);
+        textView.setText("This is a custom toast");
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
     }
 }
